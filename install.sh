fa8c4237bf8ad07b8a4a39b6945174dfbf887657
Zachary Ferguson <zfergus2@gmu.edu>
2016-09-24 17:36:35 -0400

    Mimetypes

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 # Install Theme-Mine by adding symbolic links in the neccessary folders.
 
 # Install GTK-theme
-ln -s ./numix-mine/ /usr/share/themes/Numix-Mine
+ln -sf ./numix-mine/ /usr/share/themes/Numix-Mine
 
 # Install Icon theme
-ln -s ./flattr-mine/ /usr/share/icons/flattr-mine
+ln -sf ./flattr-mine/ /usr/share/icons/flattr-mine
+
+# Add Mimetypes not included by Ubuntu
+cp ./mime-types/lisp.xml /usr/share/mime/packages/lisp.xml
+update-mime-database /usr/share/mime/
+
